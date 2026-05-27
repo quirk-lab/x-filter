8e9ea227916162e7f83c972f45506d3d5dcd7f46
Yang,Zhou <thomaszdxsn@icloud.com>
2026-05-28 04:44:01 +0800

    chore: count package sources in coverage

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,17 @@ const apps = new Set(['playground', 'web']);
 const makeProject = (name) => {
   const isApp = apps.has(name);
   const rootDir = path.join(__dirname, isApp ? 'apps' : 'packages', name);
+  const collectCoverageFrom = isApp
+    ? []
+    : [
+        'src/**/*.{ts,tsx}',
+        '!src/**/*.d.ts',
+        '!src/**/__tests__/**',
+        '!src/**/*.spec.ts',
+        '!src/**/*.spec.tsx',
+        '!src/**/*.test.ts',
+        '!src/**/*.test.tsx',
+      ];
 
   return {
     displayName: name,
@@ -17,6 +28,7 @@ const makeProject = (name) => {
       '^@x-filter/(.*)$': path.join(__dirname, 'packages/$1/src'),
     },
     setupFilesAfterEnv: jsdomProjects.has(name) ? [path.join(__dirname, 'jest.setup.ts')] : [],
+    collectCoverageFrom,
     coveragePathIgnorePatterns: ['/node_modules/'],
   };
 };
